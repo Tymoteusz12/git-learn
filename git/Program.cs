d62a1d3a33b54f4621178ac76b4274595e3201b3
Tymoteusz Jagiello <tymoteusz.jagiello00@gmail.com>
2021-07-11 16:33:00 +0200

    Change text in console writeline

--- git/Program.cs
+++ git/Program.cs
@@ -6,7 +6,7 @@ namespace git
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello mordo!");
         }
     }
 }
